91cfdd32b89f37491ef385d357dcdd23f4a266dd
Norman <norman.rudo@gmail.com>
2018-05-28 19:23:50 +0200

    added SCSS to projekt

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
-<?php include 'core/metaParams.php'; ?>
+<?php include 'sites/core/metaParams.php'; ?>
 <body>
-<?php include 'components/header.php'; ?>
+<?php include 'sites/components/header.php'; ?>
 
 <h1>My First Heading</h1>
 <p>My first paragraph.</p>
