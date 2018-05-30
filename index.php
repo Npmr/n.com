8cb2d728be8f70cda394d9f8786c562602541f94
Norman <norman.rudo@gmail.com>
2018-05-30 22:43:49 +0200

    adde develop branch

--- index.php
+++ index.php
@@ -7,5 +7,14 @@
 <h1>My First Heading</h1>
 <p>My first paragraph.</p>
 
+<form action="loginAction" id="loginForm">
+      <label>User name:&nbsp;</label>
+      <input type="text" name="username"><br>
+      <label>Password:&nbsp;</label>
+      <input type="text" name="password"><br>
+      <button type="submit" id="loginButton">Log In</button>
+      <button type="reset" id="reset">Clear</button>
+    </form>
+
 </body>
 </html>
