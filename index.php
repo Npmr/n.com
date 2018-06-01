491d9f05983147f13352fee3633df57d4d89bed1
Norman <norman.rudo@gmail.com>
2018-06-01 14:37:15 +0200

    added first part for selenium and scss part and styleguide

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <?php include 'sites/core/metaParams.php'; ?>
 <body>
 <?php include 'sites/components/header.php'; ?>
-
+<br><br><br><br><br><br><br><br>
 <h1>My First Heading</h1>
 <p>My first paragraph.</p>
-
+<br><br><br><br><br><br><br><br>
 <form action="loginAction" id="loginForm">
       <label>User name:&nbsp;</label>
       <input type="text" name="username"><br>
--- sites/components/header.php
+++ sites/components/header.php
@@ -1,3 +1,3 @@
 <div class="header">
-  <p>NpmR</p>
+  <a href="index.php">NpmR</a>
 </div>
