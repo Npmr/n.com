5f08b016b813585182ce44610d656bda1379f07b
Norman <norman.rudo@gmail.com>
2018-06-03 00:40:58 +0200

    added new core scss files and worked on header file

--- sites/components/header.php
+++ sites/components/header.php
@@ -1,3 +1,12 @@
 <div class="header">
-  <a href="index.php">NpmR</a>
+  <div class="">
+    <p>left</p>
+    <a href="index.php">NpmR</a>
+  </div>
+  <div class="header_elements_right">
+    <p>right</p>
+    <p>right 2</p>
+    <p>right 3</p>
+  </div>
+
 </div>
